0f888ef975720536c53e67c1d49c515dd2126093
BitBoy3 <repo.bit-boy3@gmail.com>
2014-05-22 16:07:24 -0500

    Added DrawGrid that draws a 20x20 grid of 'O'

--- GoL.py
+++ GoL.py
@@ -2,18 +2,23 @@
 Conway's Game of Life
 http://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules
 Rules: 
-Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-Any live cell with two or three live neighbours lives on to the next generation.
-Any live cell with more than three live neighbours dies, as if by overcrowding.
-Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+2. Any live cell with two or three live neighbours lives on to the next generation.
+3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 """
-class Game(object):
-	"""docstring for Game"""
-	def __init__(self, arg):
-		super(Game, self).__init__()
-		self.arg = arg
 CellsDict = {}
 from random import randint
+
+grid = []
+for i in range(20):
+    grid.append('O' * 20)
+def DrawGrid(grid):
+    for row in grid:
+        print ''.join(row)
+
+DrawGrid(grid)
+
 def MakeBoard(x, y):
     for item in range(x):
         for i in range(y):
