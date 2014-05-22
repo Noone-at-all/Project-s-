828f003f7160e2c4eb77fec03908f0a527f14263
Noone-at-all <Probablereply@gmail.com>
2014-05-22 10:49:20 -0500

    Update GoL.py

--- GoL.py
+++ GoL.py
@@ -7,6 +7,13 @@ Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overcrowding.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 """
+File = open('CellList.txt', 'w')
+def MakeBoard(x, y):
+#Makes the cells we need for the game.
+    for item in range(x):
+        for i in range(y):
+            File.write('Cell(%s, %s) = Cell(%s, %s)' % (str(item), str(i), int(item), int(i)))
+            File.write('\n')
 class Game(object):
 	"""docstring for Game"""
 	def __init__(self, arg):
@@ -14,16 +21,13 @@ class Game(object):
 		self.arg = arg
 
 class Cell(object):
-  	def __init__(self, position):
-        self.position = position 
-	class LivingCell(Cell):
-    	IsAlive = True
-    	def __init__(self, position):
-        	self.position = position
-        	self.IsAlive = IsAlive
-	class DeadCell(Cell)
-    	IsAlive = False
-    	def __init__(self, position):
-        	self.position = position
-        	self.IsAlive = IsAlive
-
+    def __init__(self, position):
+        self.position = position
+    def isalive(self):
+    	
+class LivingCell(Cell):
+    def __init__(self, position):
+        self.position = position
+class DeadCell(Cell)
+    def __init__(self, position):
+        self.position = position
