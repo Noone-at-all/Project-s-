a78e185a63b3b7bce74c108bcb59814b1f0f00ed
BitBoy3 <openlinuxrules@gmail.com>
2014-05-21 23:10:09 -0500

    Update GoL.py
    Added rules of GoL to code as comments.

--- GoL.py
+++ GoL.py
@@ -5,3 +5,9 @@ Testing 123.... THIS IS BIT-BOY
 You are confirmed for working.
 Yes, yes you can, but you might overwrite someone else's changes.
 """
+# Conway's Game of Life
+# Rules: 
+# Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+# Any live cell with two or three live neighbours lives on to the next generation.
+# Any live cell with more than three live neighbours dies, as if by overcrowding.
+# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
