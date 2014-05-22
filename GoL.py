1f1c9f4d349bf108ac1b0149163c5c0b818cbb6c
Noone-at-all <Probablereply@gmail.com>
2014-05-21 23:14:26 -0500

    Update GoL.py

--- GoL.py
+++ GoL.py
@@ -1,13 +1,12 @@
 """
-Guise, we settled on game of life. right?
-(If so, edit this so each of knows what was going on.)
-Testing 123.... THIS IS BIT-BOY
-You are confirmed for working.
-Yes, yes you can, but you might overwrite someone else's changes.
+Conway's Game of Life
+http://en.wikipedia.org/wiki/Conway's_Game_of_Life#Rules
+Rules: 
+Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+Any live cell with two or three live neighbours lives on to the next generation.
+Any live cell with more than three live neighbours dies, as if by overcrowding.
+Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 """
-# Conway's Game of Life
-# Rules: 
-# Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-# Any live cell with two or three live neighbours lives on to the next generation.
-# Any live cell with more than three live neighbours dies, as if by overcrowding.
-# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+class Cell(object):
+    def __init__(self):
+        pass
