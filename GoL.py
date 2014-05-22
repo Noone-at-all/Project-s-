9dfc66a5960f03c128e7278dc5e09ceec3c8bdce
Noone-at-all <Probablereply@gmail.com>
2014-05-22 11:38:35 -0500

    Update GoL.py

--- GoL.py
+++ GoL.py
@@ -8,26 +8,34 @@ Any live cell with more than three live neighbours dies, as if by overcrowding.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 """
 File = open('CellList.txt', 'w')
-def MakeBoard(x, y):
-#Makes the cells we need for the game.
-    for item in range(x):
-        for i in range(y):
-            File.write('Cell(%s, %s) = Cell(%s, %s)' % (str(item), str(i), int(item), int(i)))
-            File.write('\n')
 class Game(object):
 	"""docstring for Game"""
 	def __init__(self, arg):
 		super(Game, self).__init__()
 		self.arg = arg
-
+CellsDict = {}
+def MakeBoard(x, y):
+    for item in range(x):
+        for i in range(y):
+            File.write('Cell(%s, %s) = Cell(%s, %s)' % (str(item), str(i), int(item), int(i)))
+            File.write('\n')
+            CellsDict[item, i] = Cell(item, i)
 class Cell(object):
-    def __init__(self, position):
-        self.position = position
+    def __init__(self, x, y):
+        self.positionx = x
+        self.positiony = y
+    def CheckLife(self):
+        self,life = 'You\'re checking, and I\'m still between dead and alive.'
+    def __repr__(self):
+        return '(%s, %s)' % (str(self.positionx), str(self.positiony))
     def isalive(self):
-    	
+        pass
+#A template cell.
 class LivingCell(Cell):
-    def __init__(self, position):
-        self.position = position
-class DeadCell(Cell)
-    def __init__(self, position):
-        self.position = position
+    def __repr__(self):
+        return '(%s, %s), %s' % (str(self.positionx), str(self.positiony), str(self.life))
+#Living cells, soon to be improved.
+class DeadCell(Cell):
+    def __repr__(self):
+        return '(%s, %s), %s' % (str(self.positionx), str(self.positiony), str(self.life))
+#Dead cells, soon to be living.
