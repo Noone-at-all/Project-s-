43486e65ebed2ef720ba7e0320c233effee1d51d
BitBoy3 <repo.bit-boy3@gmail.com>
2014-05-21 23:25:26 -0500

    Setup of Game class

--- GoL.py
+++ GoL.py
@@ -7,6 +7,12 @@ Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overcrowding.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 """
+class Game(object):
+	"""docstring for Game"""
+	def __init__(self, arg):
+		super(Game, self).__init__()
+		self.arg = arg
+
 class Cell(object):
     def __init__(self):
         pass
