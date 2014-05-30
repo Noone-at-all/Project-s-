1ce11e512d89942d48e7b0ea216943b49c7b2fd1
madewithlinux <madewithlinux.josh@gmail.com>
2014-05-30 14:59:04 -0500

    fixed run full screen script

--- RunFullScreen.py
+++ RunFullScreen.py
@@ -3,11 +3,11 @@ import GoL
 from time import sleep
 import os
 rows, columns = os.popen('stty size', 'r').read().split()
-rows = int(rows)-1
-columns = int(columns)
+rows = int(rows)/2 -1
+columns = int(columns)/2
 brd = ""
 while True:
-	brd = GoL.Board(rows,columns)
+	brd = GoL.Board(columns,rows)
 	brd.IconNoLife = " "
 	# for x in xrange(500):
 	while True:
